a0324c05acf9b4a2efc26de0cfce87f6c15e18b9
Karguvelrajan Palaniselvam <rajan.p@ebenefitsnetwork.com>
2019-06-18 12:06:08 +0530

    Image changed and added meta data

--- routes/data.js
+++ routes/data.js
@@ -11,141 +11,140 @@ const data = {
         email: 'rylmotorschennai@gmail.com',
         mapLocation: '12.921737, 80.140272'
     },
-    servicesOffered: [
-        {
+    servicesOffered: [{
             title: 'Package I',
             cost: 450,
             description: 'Water Service, 3M Polish and Oil Lubrication',
             services: [
-                'Water Service', 
+                'Water Service',
                 '3M Polish',
                 'Oil Lubrication'
             ],
             otherInstructions: 'You can give some special notice here',
             code: 'package001',
-            imageLink: 'images/motors/IMG-20190603-WA0005.jpg'
+            imageLink: 'images/LatestImage/IMG_0206.jpg'
         },
         {
             title: 'Package II',
             cost: 1699,
             description: 'Engine Oil, Oil Filter, Water wash, Oil Lubrication with Polish',
             services: [
-                'Engine Oil', 
+                'Engine Oil',
                 'Oil Filter',
                 'Water wash',
                 'Oil Lubrication with Polish'
             ],
             otherInstructions: '',
             code: 'package002',
-            imageLink: 'images/motors/IMG-20190603-WA0002.jpg'
+            imageLink: 'images/LatestImage/IMG_0200.jpg'
         },
         {
             title: 'General Service I',
             cost: 999,
             description: 'General Service For Scooters',
             services: [
-                'Engine Oil', 
+                'Engine Oil',
                 'Oil Filter',
                 'Water wash',
                 'Oil Lubrication with Polish'
             ],
             otherInstructions: '',
             code: 'generalservice001',
-            imageLink: 'images/motors/scooter.jpg'
+            imageLink: 'images/LatestImage/IMG_0218.jpg'
         },
         {
             title: 'General Service II',
             cost: 1499,
             description: 'General Service For 100cc to 180cc Bikes',
             services: [
-                'Engine Oil', 
+                'Engine Oil',
                 'Oil Filter',
                 'Water wash',
                 'Oil Lubrication with Polish'
             ],
             otherInstructions: '',
             code: 'generalservice002',
-            imageLink: 'images/motors/IMG-20190603-WA0011.jpg'
+            imageLink: 'images/LatestImage/IMG_0211.jpg'
         },
         {
-            title: 'Premium Service',
+            title: 'Premium Service  I',
             cost: 1999,
             description: 'Premium Service For RE Bikes',
             services: [
-                'Engine Oil', 
+                'Engine Oil',
                 'Air Filter',
-                'Electrical checkup', 
-                'Carburetor Cleaning', 
-                'Engine Tuning', 
-                'Oil Lubrications', 
-                'Brakes Cleaning', 
-                'Water Wash', 
+                'Electrical checkup',
+                'Carburetor Cleaning',
+                'Engine Tuning',
+                'Oil Lubrications',
+                'Brakes Cleaning',
+                'Water Wash',
                 '3M Polish'
             ],
             otherInstructions: '',
             code: 'premiumservice001',
-            imageLink: 'images/motors/IMG-20190603-WA0010.jpg'
+            imageLink: 'images/LatestImage/IMG_0189.jpg'
         },
         {
             title: 'Premium Service II',
             cost: 4199,
             description: 'Premium Service For RE Bikes with Sprocket Change',
             services: [
-                'Engine Oil', 
+                'Engine Oil',
                 'Air Filter',
-                'Electrical checkup', 
-                'Carburetor Cleaning', 
-                'Engine Tuning', 
-                'Oil Lubrications', 
-                'Brakes Cleaning', 
-                'Water Wash', 
+                'Electrical checkup',
+                'Carburetor Cleaning',
+                'Engine Tuning',
+                'Oil Lubrications',
+                'Brakes Cleaning',
+                'Water Wash',
                 '3M Polish',
                 'Sprocket Change'
             ],
             otherInstructions: '',
             code: 'premiumservice002',
-            imageLink: 'images/motors/IMG-20190603-WA0005.jpg'
+            imageLink: 'images/LatestImage/IMG_0183.jpg'
         },
         {
             title: 'Full Restore I',
             cost: 29999,
             description: 'Single Color Painting and Plating',
             services: [
-                'Engine Oil', 
+                'Engine Oil',
                 'Air Filter',
-                'Electrical checkup', 
-                'Carburetor Cleaning', 
-                'Engine Tuning', 
-                'Oil Lubrications', 
-                'Brakes Cleaning', 
-                'Water Wash', 
+                'Electrical checkup',
+                'Carburetor Cleaning',
+                'Engine Tuning',
+                'Oil Lubrications',
+                'Brakes Cleaning',
+                'Water Wash',
                 '3M Polish',
                 'Sprocket Change'
             ],
             otherInstructions: '',
             code: 'fullrestore001',
-            imageLink: 'images/motors/motor_headlight.jpg'
+            imageLink: 'images/LatestImage/IMG_0198.jpg'
         },
         {
             title: 'Full Restore II',
             cost: 49999,
             description: 'Full Restore with Engine Work, Painting, Plating',
             services: [
-                'Engine Oil', 
+                'Engine Oil',
                 'Air Filter',
-                'Electrical checkup', 
-                'Carburetor Cleaning', 
-                'Engine Tuning', 
-                'Oil Lubrications', 
-                'Brakes Cleaning', 
-                'Water Wash', 
+                'Electrical checkup',
+                'Carburetor Cleaning',
+                'Engine Tuning',
+                'Oil Lubrications',
+                'Brakes Cleaning',
+                'Water Wash',
                 '3M Polish',
                 'Sprocket Change',
                 'Engine Work'
             ],
             otherInstructions: '',
             code: 'fullrestore002',
-            imageLink: 'images/motors/engine.jpg'
+            imageLink: 'images/LatestImage/IMG_0177.jpg'
         }
     ]
 }
