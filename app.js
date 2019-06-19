d7a1b0d7c82a0c56a176db6a1f6acd3aaf781261
Karguvelrajan Palaniselvam <rajan.p@ebenefitsnetwork.com>
2019-06-19 11:35:37 +0530

    Changes updated

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ var usersRouter = require('./routes/users');
 var servicesRouter = require('./routes/services');
 var contactRouter = require('./routes/contact');
 var thanksRouter = require('./routes/thanks');
-
 var apiRouter = require('./routes/api');
+var sparesRouter = require('./routes/spares');
 
 var app = express();
 
@@ -33,22 +33,23 @@ app.use('/services', servicesRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 app.use('/thanks', thanksRouter);
+app.use('/spares', sparesRouter);
 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
